874b097e4e02e846a890f93511db08cbea19bdbc
Umair <Umair@DELL04.fios-router.home>
2019-08-07 06:11:21 -0400

    First Load

---
+++ src/API.java
@@ -0,0 +1,25 @@
+
+public class API {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+    	//System.out.println("Loading Success Sufian Rashidi...");
+		String val1 = "-";
+		String ev01 = "Y";
+
+    	if(ev01 == "Y" && !val1.equals("-")) {
+			
+        	System.out.println(val1);
+
+			
+		
+			
+	}else {
+    	System.out.println("fool");
+
+		
+	}
+	}
+
+}
